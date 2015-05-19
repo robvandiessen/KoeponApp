8cf8d49807b7910bf964229ef8ce688358bcdfc4
MIAGIT <nick_coevering@hotmail.com>
2015-05-19 16:30:11 +0200

    Plugins
    Lijst met geinstalleerde plugins
    - iBeacon
    - Vibration
    - Dialogs
    - Toast
    - BackgroundGeoLocation
    
    Details te vinden @
    https://docs.google.com/document/d/1ebSrvgG8Uq8af7m0cT5ibXZyfzkqkhVlLNnF6ZKD48Y/edit#

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,81 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/eu.mobilion.ibeacon/www/ibeacon.js",
+        "id": "eu.mobilion.ibeacon.ibeacon",
+        "clobbers": [
+            "ibeacon"
+        ]
+    },
+    {
+        "file": "plugins/eu.mobilion.ibeacon/www/region.js",
+        "id": "eu.mobilion.ibeacon.region"
+    },
+    {
+        "file": "plugins/eu.mobilion.ibeacon/www/beacon.js",
+        "id": "eu.mobilion.ibeacon.beacon"
+    },
+    {
+        "file": "plugins/eu.mobilion.ibeacon/www/helper.js",
+        "id": "eu.mobilion.ibeacon.helper"
+    },
+    {
+        "file": "plugins/eu.mobilion.ibeacon/www/defaults.js",
+        "id": "eu.mobilion.ibeacon.defaults"
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.toast/www/Toast.js",
+        "id": "nl.x-services.plugins.toast.Toast",
+        "clobbers": [
+            "window.plugins.toast"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.toast/test/tests.js",
+        "id": "nl.x-services.plugins.toast.tests"
+    },
+    {
+        "file": "plugins/org.transistorsoft.cordova.background-geolocation/www/BackgroundGeoLocation.js",
+        "id": "org.transistorsoft.cordova.background-geolocation.BackgroundGeoLocation",
+        "clobbers": [
+            "plugins.backgroundGeoLocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.vibration": "0.3.13",
+    "eu.mobilion.ibeacon": "0.0.0",
+    "nl.x-services.plugins.toast": "2.0.4",
+    "org.transistorsoft.cordova.background-geolocation": "0.3.6",
+    "cordova-plugin-geolocation": "1.0.0",
+    "cordova-plugin-dialogs": "1.1.0"
 }
 // BOTTOM OF METADATA
 });
---
+++ platforms/android/assets/www/plugins/eu.mobilion.ibeacon/www/defaults.js
@@ -0,0 +1,9 @@
+cordova.define("eu.mobilion.ibeacon.defaults", function(require, exports, module) { 'use strict';
+
+module.exports = {
+
+  identifier: 'default-identifier',
+
+};
+
+});
