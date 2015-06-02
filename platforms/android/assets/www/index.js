8b70534b3c3cb47131892f3edf83c30dfa0bb047
MIAGIT <nick_coevering@hotmail.com>
2015-06-02 14:57:53 +0200

    Phonon Framework & MaterializeCSS toegevoegd
    Alle bestanden toegevoegd, opzet navigatie toegevoegd, fastclick
    toegevoegd en ondersteuning voor hardwarematige terugknop toegevoegd.
    Eerste testpagina werkend, en de naam van de app ingesteld

--- platforms/android/assets/www/index.js
+++ platforms/android/assets/www/index.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+//--App var--//
 var app = {
     // Application Constructor
     initialize: function() {
@@ -47,5 +30,54 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-
 app.initialize();
+
+//--dependencies, utilities & eventlisteners--//
+//Load fastclick
+$(function() {
+    FastClick.attach(document.body);
+});
+
+//hardware back button
+function onDeviceReady() {
+    // Register the event listener
+    document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+//start the navigation
+var onDeviceReady = function () {
+	Phonon.Navigator().start('home');
+};
+document.addEventListener('deviceready', onDeviceReady, false);
+
+//hardware back button eventhandler
+function onBackKeyDown() {
+    
+}
+
+//--Navigation--//
+Phonon.Navigator({
+    defaultPage: 'home',
+    templatePath: 'tpl',
+    pageAnimations: true
+});
+
+//Home
+Phonon.Navigator().on({page: 'home', template: 'home', asynchronous: false}, function(activity) {
+    //Here you can call functions on page load, quit etc
+    activity.onCreate(function(self, el, req) {
+
+    });
+    activity.onReady(function(self, el, req) {
+
+    });
+    activity.onTransitionEnd(function() {
+
+    });
+    activity.onQuit(function(self) {
+
+    });
+    activity.onHidden(function(el) {
+
+    });
+});
---
+++ www/index.js
@@ -0,0 +1,83 @@
+//--App var--//
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+app.initialize();
+
+//--dependencies, utilities & eventlisteners--//
+//Load fastclick
+$(function() {
+    FastClick.attach(document.body);
+});
+
+//hardware back button
+function onDeviceReady() {
+    // Register the event listener
+    document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+//start the navigation
+var onDeviceReady = function () {
+	Phonon.Navigator().start('home');
+};
+document.addEventListener('deviceready', onDeviceReady, false);
+
+//hardware back button eventhandler
+function onBackKeyDown() {
+    Phonon.Navigator().changePage(getPreviousPage());
+}
+
+//--Navigation--//
+Phonon.Navigator({
+    defaultPage: 'home',
+    templatePath: 'tpl',
+    pageAnimations: true
+});
+
+//Home
+Phonon.Navigator().on({page: 'home', template: 'home', asynchronous: false}, function(activity) {
+    //Here you can call functions on page load, quit etc
+    activity.onCreate(function(self, el, req) {
+
+    });
+    activity.onReady(function(self, el, req) {
+
+    });
+    activity.onTransitionEnd(function() {
+
+    });
+    activity.onQuit(function(self) {
+
+    });
+    activity.onHidden(function(el) {
+
+    });
+});
